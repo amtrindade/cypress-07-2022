37ea0cefca2bf722b021289c35c57759b79d0193
Antonio Trindade <atrindade@umov.me>
2022-07-11 20:31:09 -0300

    Checkbox

--- cypress/integration/webelements.spec.js
+++ cypress/integration/webelements.spec.js
@@ -36,10 +36,32 @@ describe('Should be validate title page', () => {
 
         it('Work with RadioButton', () => {
             cy.visit("/elementsweb.html");
+            
             cy.get("[name='radioGroup1']").first().check().should("be.checked");
             cy.get("[name='radioGroup1']").last().check().should("be.checked");
             cy.get("[name='radioGroup1']").check("Radio 3").should("be.checked");
             cy.get("[name='radioGroup1']").last().should("not.be.checked");
+
+            cy.get("[name='radioGroup1']").should("have.length", 4);
+        })
+
+        it.only('Work with CheckBox', () => {
+            cy.visit("/elementsweb.html");
+
+            cy.get("[name='chkbox']").should("have.length", 4); 
+            cy.get("[name='chkbox']")
+                .check("Check 2")
+                .should("be.checked")
+                .uncheck("Check 2")
+                .should("not.be.checked");
+            
+            cy.get("[name='chkbox']").check( {multiple: true} );
+            cy.get("[name='chkbox']").first().should("be.checked");
+            cy.get("[name='chkbox']").should("be.checked");
+            cy.get("[name='chkbox']").first().uncheck().should("not.be.checked");
+            
+            //Não validou todos checados da forma como deveria
+            cy.get("[name='chkbox']").should("be.checked");
         })
     })
 })
