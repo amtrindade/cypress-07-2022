edcb86e8c05a6db970bcf1cdc482a61d3cd16c78
Antonio Trindade <atrindade@umov.me>
2022-07-12 20:09:24 -0300

    Exclude task

--- cypress/integration/todo.elementos.spec.js
+++ cypress/integration/todo.elementos.spec.js
@@ -26,7 +26,38 @@ describe("Todo Spec", () => {
             .parent().find('input').check().should('be.checked');
     })
 
+    it('Should validate complete task', () => {
+        cy.visit("https://example.cypress.io/todo");
 
+        const task = "Fix the bug";
 
+        cy.get('[data-test="new-todo"]').type(`${task}{enter}`);
+        cy.get('.todo-list').contains(`${task}`)
+            .parent().find('input').check().should('be.checked');
+
+        cy.get('.filters').contains('Completed').click();
+        cy.get('.todo-list li').should("have.length", 1);
+        cy.get('.todo-list li').should("have.text", `${task}`);
+    })
+
+    it.only('Should delete new task', () => {
+        cy.visit("https://example.cypress.io/todo");
+
+        const task = "Fix the bug";
+
+        cy.get('[data-test="new-todo"]').type(`${task}{enter}`);
+        cy.get('.todo-list').contains(`${task}`)
+            .parent().find('input').check().should('be.checked');
+
+        cy.get('.filters').contains('Completed').click();
+
+        cy.get('.todo-list').contains(`${task}`)
+            .parent().find('.destroy').click({force: true});
+
+        cy.get('.filters').contains('All').click();
+
+        cy.get('.todo-list li').should("have.length", 2);
+        cy.get('.todo-list li').should("not.have.text", `${task}`);
+    })
 
 })
