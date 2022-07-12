b6b1eaff6ece60c3fe6fb3430c7e0b787abacfa5
Antonio Trindade <atrindade@umov.me>
2022-07-12 20:46:49 -0300

    Todo list

--- cypress/integration/todo.elementos.spec.js
+++ cypress/integration/todo.elementos.spec.js
@@ -1,10 +1,11 @@
 /// <reference types="cypress" />
 
 describe("Todo Spec", () => {
+    beforeEach(() => {
+        cy.visit("https://example.cypress.io/todo");
+    })
 
     it('Should include new task', () => {
-        cy.visit("https://example.cypress.io/todo");
-
         const task = "Fix the bug";
 
         cy.get('.todo-list li').should("have.length", 2);
@@ -15,49 +16,40 @@ describe("Todo Spec", () => {
 
     })
 
-    it('Should check task', () => {
-        cy.visit("https://example.cypress.io/todo");
-
+    describe('Tests with task add', () => {
         const task = "Fix the bug";
 
-        cy.get('[data-test="new-todo"]').type(`${task}{enter}`);
-        //cy.get('.todo-list li').last().find('input').check();
-        cy.get('.todo-list').contains(`${task}`)
-            .parent().find('input').check().should('be.checked');
+        beforeEach(() => {
+            cy.get('[data-test="new-todo"]').type(`${task}{enter}`);
+        })
+
+        it('Should check task', () => {
+            //cy.get('.todo-list li').last().find('input').check();
+            cy.get('.todo-list').contains(`${task}`)
+                .parent().find('input').check().should('be.checked');
+        })
+
+        it('Should validate complete task', () => {
+            cy.get('.todo-list').contains(`${task}`)
+                .parent().find('input').check().should('be.checked');
+
+            cy.get('.filters').contains('Completed').click();
+            cy.get('.todo-list li').should("have.length", 1);
+            cy.get('.todo-list li').should("have.text", `${task}`);
+        })
+
+        it('Should delete new task', () => {
+            cy.get('.todo-list').contains(`${task}`)
+                .parent().find('input').check().should('be.checked');
+            cy.get('.filters').contains('Completed').click();
+
+            cy.get('.todo-list').contains(`${task}`)
+                .parent().find('.destroy').click({force: true});
+            cy.get('.filters').contains('All').click();
+
+            cy.get('.todo-list li').should("have.length", 2);
+            cy.get('.todo-list li').should("not.have.text", `${task}`);
+        })
+
     })
-
-    it('Should validate complete task', () => {
-        cy.visit("https://example.cypress.io/todo");
-
-        const task = "Fix the bug";
-
-        cy.get('[data-test="new-todo"]').type(`${task}{enter}`);
-        cy.get('.todo-list').contains(`${task}`)
-            .parent().find('input').check().should('be.checked');
-
-        cy.get('.filters').contains('Completed').click();
-        cy.get('.todo-list li').should("have.length", 1);
-        cy.get('.todo-list li').should("have.text", `${task}`);
-    })
-
-    it.only('Should delete new task', () => {
-        cy.visit("https://example.cypress.io/todo");
-
-        const task = "Fix the bug";
-
-        cy.get('[data-test="new-todo"]').type(`${task}{enter}`);
-        cy.get('.todo-list').contains(`${task}`)
-            .parent().find('input').check().should('be.checked');
-
-        cy.get('.filters').contains('Completed').click();
-
-        cy.get('.todo-list').contains(`${task}`)
-            .parent().find('.destroy').click({force: true});
-
-        cy.get('.filters').contains('All').click();
-
-        cy.get('.todo-list li').should("have.length", 2);
-        cy.get('.todo-list li').should("not.have.text", `${task}`);
-    })
-
 })
