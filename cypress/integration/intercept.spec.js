10d6cd7ae4d1a91b8798d5269a44defa72538c47
Antonio Trindade <atrindade@umov.me>
2022-07-20 21:18:05 -0300

    Include intercerp request

--- cypress/integration/intercept.spec.js
+++ cypress/integration/intercept.spec.js
@@ -3,6 +3,7 @@
 const requiredExample = require('../fixtures/example')
 
 context('Files', () => {
+
   beforeEach(() => {
     cy.visit('https://example.cypress.io/commands/files')
   })
@@ -19,5 +20,19 @@ context('Files', () => {
     cy.wait('@getComment').its('response.body')
       .should('have.property', 'name')
       .and('include', 'Using fixtures to represent data')
+  })
+
+  it('get normal', () => {
+    cy.request({
+        url: `https://jsonplaceholder.cypress.io/comments/1`,
+        method: 'GET'
+      }).as('getComment');
+
+
+
+    cy.get('@getComment').then(response => {
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.have.property('name', 'id labore ex et quam laborum');
+    })
   })
 })
